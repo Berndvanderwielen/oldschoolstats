aacb1a8762a7a7075305749d25b07eab7ea8f4b0
Berndvanderwielen <23399958+Berndvanderwielen@users.noreply.github.com>
2021-02-22 21:20:51 +0100

    add example & small tweaks

---
+++ example.py
@@ -0,0 +1,5 @@
+from oldschoolstats import Hiscores
+
+hs = Hiscores()
+data = hs.getUser("Dieptepunt")
+print(data)
--- oldschoolstats/hiscores.py
+++ oldschoolstats/hiscores.py
@@ -7,40 +7,39 @@ from bs4 import BeautifulSoup
 
 
 class Hiscores:
-
     def __init__(self):
-        config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+        config_file = os.path.join(os.path.dirname(__file__), "config.ini")
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
 
-        self.base_url = self.config.get('api', 'base_url')
-        self.skills = self.config.get('data_format', 'skills').split(',')
-        self.metrics = self.config.get('data_format', 'hierachy_skills').split(',')
+        self.base_url = self.config.get("api", "base_url")
+        self.skills = self.config.get("data_format", "skills").split(",")
+        self.metrics = self.config.get("data_format", "hierachy_skills").split(",")
 
-    def getHiscorePage(self, user_type='normal', offset=0):
+    def getHiscorePage(self, user_type="normal", offset=0):
         url = "{}{}/overall.ws?table=0&page={}".format(
-            self.base_url,
-            self.config.get('types', user_type),
-            offset)
+            self.base_url, self.config.get("types", user_type), offset
+        )
 
-        player = namedtuple('User', ['rank', 'displayname', 'level', 'xp'])
+        player = namedtuple("User", ["rank", "displayname", "level", "xp"])
         players = []
         raw = requests.get(url).text
-        soup = BeautifulSoup(raw, 'html.parser')
-        for row in soup.find_all('tr', {'class': 'personal-hiscores__row'}):
-            cols = [unidecode(x.text.strip()) for x in row.find_all('td')]
-            cols = [x.replace(',', '') for x in cols]
+        soup = BeautifulSoup(raw, "html.parser")
+        for row in soup.find_all("tr", {"class": "personal-hiscores__row"}):
+            cols = [unidecode(x.text.strip()) for x in row.find_all("td")]
+            cols = [x.replace(",", "") for x in cols]
             cols = [int(col) if cols.index(col) in [0, 2, 3] else col for col in cols]
             players.append(player(*cols))
         return players
 
-    def getUser(self, displayname, user_type='normal'):
+    def getUser(self, displayname, user_type="normal", output="json"):
         url = "{}{}/{}?player={}".format(
             self.base_url,
-            self.config.get('types', user_type),
-            self.config.get('api', 'endpoint'),
-            displayname)
-        data = requests.get(url.format(displayname)).text.split('\n')
+            self.config.get("types", user_type),
+            self.config.get("api", "endpoint"),
+            displayname,
+        )
+        data = requests.get(url.format(displayname)).text.split("\n")
 
         obj = {}
         for i, skill in enumerate(self.skills):
